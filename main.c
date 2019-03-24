48c70a578d477203ffd89b12c032d70749a935bf
Mayyli <maaaayyilmaz@gmail.com>
2019-03-24 20:32:50 +0300

    Hackerrank ödev

--- main.c
+++ main.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "for.h"
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+//    generateRandomMatrix("matrix1.txt", 1, 100);
+//    readMatrixFromFile("matrix1.txt");
+//    for (int i = 0; i < ROW_SIZE; ++i) {
+//        for (int j = 0; j < COLUMN_SIZE; ++j) {
+//            printf("%d\t", matrix[i][j]);
+//        }
 
 
 int main() {
-    generateRandomMatrix("matrix1.txt", 1, 100);
-    readMatrixFromFile("matrix1.txt");
-    for (int i = 0; i < ROW_SIZE; ++i) {
-        for (int j = 0; j < COLUMN_SIZE; ++j) {
-            printf("%d\t", matrix[i][j]);
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    char splintStrings[10][10];
+
+    int j,cnt;
+    j=0;
+    cnt = 0;
+
+    for(int i=0;i<=(strlen(s));i++){
+        if (s[i]==' ' || s[i] =='\0' ){
+            splintStrings[cnt][j]='\0';
+            cnt++;
+            j=0;
         }
-        printf("\n");
+        else{
+            splintStrings[cnt][j]=s[i];
+            j++;
+        }
+
     }
-    minInColumn(2);
-    maxInColumn(2);
-    minInRow(2);
-    maxInRow(2);
-    printf("%d\n", diagonalDiff());
+    for (int i=0;i<cnt;i++){
+        printf("%s\n",splintStrings[i]);
+    }
     return 0;
+
+
+
 }
+
 
 
